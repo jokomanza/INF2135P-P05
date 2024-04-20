5359fde43a62c54b0c409137e92ca894b11101d5
Joko Supriyanto <jokomanza@gmail.com>
2024-04-20 22:08:05 +0700

    Get dots coordinate from user input

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -1,20 +1,61 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("This program will calculate distance between two dots");
 
-        Dot dot1 = new Dot(1, 2);
-        Dot dot2 = new Dot(3, 4);
+        Dot dot1 = generateDot("dot 1");
+        Dot dot2 = generateDot("dot 2");
+
         System.out.println("Distance: " + calculateDistance(dot1, dot2));
     }
 
-    private static long calculateDistance(Dot dot1, Dot dot2) {
+    /**
+     * Generate dot from user input
+     *
+     * @param name dot name
+     * @return dot object
+     */
+    private static Dot generateDot(String name) {
+        System.out.println("Please input coordinate for " + name);
+
+        long x = getInput("x", false);
+        long y = getInput("y", false);
+
+        return new Dot(x, y);
+    }
+
+    /**
+     * Get input from user
+     *
+     * @param name input name
+     * @param invalidInput whether the previous input is invalid
+     * @return input from user
+     */
+    private static long getInput(String name, boolean invalidInput) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (invalidInput) {
+            System.out.print("[Invalid input, please input valid number] ");
+        }
+
+        System.out.print(name + ": ");
+        try {
+            return scanner.nextLong();
+        } catch (Exception e) {
+            return getInput(name, true);
+        }
+    }
+
+    private static double calculateDistance(Dot dot1, Dot dot2) {
         long x1 = dot1.getX();
         long y1 = dot1.getY();
         long x2 = dot2.getX();
         long y2 = dot2.getY();
-        return (long) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
 }
